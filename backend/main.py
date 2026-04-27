1373c8b44b44bd8176d02aa3ec06e164ffc666c1
talha-anwar <talhaibnanwar@gmail.com>
2026-04-27 22:14:38 +0600

    added some new origins

--- backend/main.py
+++ backend/main.py
@@ -14,7 +14,11 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=[
+        "http://localhost:3000/",
+        "http://localhost:5500/",
+        "http://127.0.0.1:5500/"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
